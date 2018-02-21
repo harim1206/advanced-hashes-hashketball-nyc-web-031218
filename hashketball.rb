dec6ff6e2aac5b7c37033f9210411f9a2996f4d1
Harim Choi <harim1206@gmail.com>
2018-02-21 19:56:20 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -330,7 +330,7 @@ def long_name_steals_a_ton?
         longest_name = player_name
         longest_name_steals = stats[:steals]
         binding.pry
-        puts("#{longest_name}'s name length: #{longest_name.length}")
+        puts("#{longest_name}'s name length: #{longest_name.length}, and steals: #{stats[:steals]}")
 
       end
     }
